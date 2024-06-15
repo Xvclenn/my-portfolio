d9e11f67163ff6a7707b0594e2d94c683a3fc86e
Xvclenn <benhhslen@gmail.com>
2024-06-15 14:35:22 +0800

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+	return (
+		<footer className="bg-gray-800 text-white text-center p-4 fixed bottom-0 w-full">
+			<p>&copy; {new Date().getFullYear()} My Portfolio</p>
+		</footer>
+	);
+};
+
+export default Footer;
